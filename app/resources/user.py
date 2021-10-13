96bb4747ab5751e0afa959942e6a178f12c3d0ee
Oleastro, Lautaro <lauchaoleastro1@gmail.com>
2021-10-13 12:21:00 -0300

    Agregar usuario terminado. Pide en el template html que se llenen todos los campos. En la logica chequea que los campos esten completos y que no exista otro usuario con mismo mail antes de agregarlo.

--- app/resources/user.py
+++ app/resources/user.py
@@ -1,4 +1,5 @@
 from flask import redirect, render_template, request, url_for, session, abort
+from flask.helpers import flash
 from app.models.user import User
 from app.helpers.auth import authenticated
 
@@ -27,6 +28,20 @@ def new():
     return render_template("user/new.html")
 
 
+def hasAllParams(params):
+    """ Metodo para chequear que el issue tiene todos los params que necesita para ser creado. """
+    lista = []
+    for param in params.keys():
+        lista.append(params.get(param) != '')
+    return all(lista)
+
+
+def alreadyExists(new_user):
+    """ Devuelve True si ya existe un usuario con el mismo mail que new_user. False caso contrario. """
+    query = User.query.filter(User.email == new_user.email).first()
+    return (query != None)
+
+
 def create():
     if not authenticated(session):
         abort(401)
@@ -34,13 +49,21 @@ def create():
     # conn = connection()
     # User.create(conn, request.form)
 
-    # creamos el usuario con los parametros del diccionario request.form
-    new_user = User(**request.form)
+    if (hasAllParams(request.form)):
+        # creamos el usuario con los parametros del diccionario request.form
+        new_user = User(**request.form)
 
-    # agregamos el usuario
-    db.session.add(new_user)
+        if (alreadyExists(new_user)):
+            flash("Ya existe un usuario con mail: '" + new_user.email + "'.")
+        else:
+            flash("Creado con éxito.")
+            # agregamos el usuario
+            db.session.add(new_user)
+            # efectuamos los cambios
+            db.session.commit()
+            return redirect(url_for("user_index"))
 
-    # efectuamos los cambios
-    db.session.commit()
+    else:
+        flash("Faltan parametros.")
 
-    return redirect(url_for("user_index"))
+    return redirect(url_for("user_new"))
