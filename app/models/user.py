5b9947b8fb79adfefabd62e4a254a9ae10aa398d
Oleastro, Lautaro <lauchaoleastro1@gmail.com>
2021-10-15 12:59:44 -0300

    se agrego la funcion destroy() para el controlador y el modelo User. chequea permiso antes de hacerlo

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,9 @@
 from sqlalchemy import Column, Integer, String, Table, ForeignKey, Boolean
-from sqlalchemy.orm import backref, relationship
+from sqlalchemy.orm import backref, query, relationship, session
+from sqlalchemy.sql.functions import user
 from app.db import db
 from app.models.role import Role
+
 
 user_has_role_table = Table('user_has_role', db.metadata,
                             Column('user_id', Integer,
@@ -35,6 +37,25 @@ class User(db.Model):
         return User.query.all()
 
     def with_email(email):
+        return User.query.filter(User.email == email).first()
+
+    def with_id(user_id):
+        return User.query.filter(User.id == user_id).first()
+
+    def already_exists(email):
         """ Devuelve True si ya existe un usuario con el mail recibido como parametro. False caso contrario. """
-        query = User.query.filter(User.email == email).first()
+        query = User.with_email(email)
         return (query != None)
+
+    def destroy(user):
+        """ Elimina un usuario de la BD. """
+        print("se quiere eliminar: {user.id}")
+        db.session.delete(user)
+        db.session.commit()
+
+    def check_permission(user, permission):
+        user_permissions = []
+        for role in user.roles:
+            for perm in role.permissions:
+                user_permissions.append(perm.name)
+        return(permission in user_permissions)
