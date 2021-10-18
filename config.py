a193d94b1f4c93894e6c6505afb76554c0cf31ce
Elias Zaccheo <eliaszaccheo91@gmail.com>
2021-10-17 22:56:48 -0300

    Config SQLALCHEMY

--- config.py
+++ config.py
@@ -23,6 +23,10 @@ class ProductionConfig(Config):
     DB_USER = environ.get("DB_USER", "grupo6")
     DB_PASS = environ.get("DB_PASS", "ZDI1Zjc4N2VlMDU2")
     DB_NAME = environ.get("DB_NAME", "grupo6")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:3306/{DB_NAME}"
+    )
 
 
 class DevelopmentConfig(Config):
