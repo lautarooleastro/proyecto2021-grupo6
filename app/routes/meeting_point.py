83f9b9d636e7815918259fcb07e2612efc195bdc
Oleastro, Lautaro <lauchaoleastro1@gmail.com>
2021-11-08 12:22:01 -0300

    detalle de punto de encuentro implementado

--- app/routes/meeting_point.py
+++ app/routes/meeting_point.py
@@ -8,3 +8,6 @@ def set_routes(app):
                      "meeting_point_new", meeting_point.new)
     app.add_url_rule("/puntos_de_encuentro",
                      "meeting_point_create", meeting_point.create, methods=["POST"])
+
+    app.add_url_rule("/puntos_de_encuentro/detalle/<int:id>",
+                     "meeting_point_detail", meeting_point.detail, methods=["GET"])
