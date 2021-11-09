d45a6e5e63382ad3880daa6f5a3f7765568d0f8e
Oleastro, Lautaro <lauchaoleastro1@gmail.com>
2021-11-09 14:16:13 -0300

    nueva carpeta para ordenar los forms

--- app/helpers/forms/meeting_point.py
+++ app/helpers/forms/meeting_point.py
@@ -2,17 +2,6 @@ from wtforms import Form, validators
 from wtforms.fields.core import BooleanField, StringField
 
 
-class SignupForm(Form):
-    first_name = StringField(u'Nombre', validators=[
-                             validators.input_required()])
-    last_name = StringField(u'Apellido', validators=[
-                            validators.input_required()])
-    email = StringField(u'E-Mail', validators=[
-                        validators.input_required()])
-    password = StringField(u'Contraseña', validators=[
-                           validators.input_required(), validators.Length(min=6, max=24)])
-
-
 class NewPointForm(Form):
     name = StringField(u'Nombre', validators=[validators.input_required()])
     adress = StringField(u'Direccion', validators=[
