805b69627e31adae894bca9b1d120040b15b2fd7
Oleastro, Lautaro <lauchaoleastro1@gmail.com>
2021-12-03 22:59:39 -0300

    se agrego el mapa en una nueva ruta, mapa vacio

---
+++ web/src/views/InfoMap.vue
@@ -0,0 +1,27 @@
+<template>
+  <l-map style="height: 400px" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker :lat-lng="markerLatLng"></l-marker>
+  </l-map>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  data() {
+    return {
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 13,
+      center: [-34.92053918330889, -57.9541949099075],
+    };
+  },
+};
+</script>
