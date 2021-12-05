6f97fed68d6a0bbde6ae02b20a69f7ade93f4397
Elias Zaccheo <eliaszaccheo91@gmail.com>
2021-12-05 17:10:37 -0300

    Editar arreglado

--- app/resources/flood_zone.py
+++ app/resources/flood_zone.py
@@ -4,6 +4,7 @@ from flask.helpers import flash, url_for
 from wtforms.validators import ValidationError
 from app.helpers.permission import permission_required
 from app.models.flood_zone import FloodZone
+from app.models.flood_point import FloodPoint
 from wtforms import form
 from app.helpers.forms.flood_zone import NewFloodZoneForm
 from app.helpers.flood_zone_csv import getZones
@@ -60,76 +61,79 @@ def create():
             flash("Nombre de zona ya existente", "error")
             return redirect(url_for("flood_zone_new"))
 
-    data = request.form
-    if (form.validate()):
-        data.to_dict(flat=False)
-        flood_zone= FloodZone()
-        form.populate_obj(flood_zone)
-        flood_zone.color=flood_zone.color.replace("#","").upper()  #revisar, el caracter # debe ser eliminado antes
-        flood_zone.save()
-        if FloodZone.with_id(flood_zone.id)!= None:
-            flash("Se creó la zona "+flood_zone.code+" correctamente", "success")
-            return redirect(url_for("flood_zone_index"))
-        else:
-            flash("No fue posible crear la zona", "error")
-            return redirect(url_for("flood_zone_new"))
-        
+    
+    flood_zone=_newZone(form)
+    flood_zone.save()
+    if FloodZone.with_id(flood_zone.id)!= None:
+        flash("Se creó la zona "+flood_zone.code+" correctamente", "success")
+        return redirect(url_for("flood_zone_index"))
     else:
-        for error in form.errors:
-            flash(form.errors[error], "error")
-    return redirect(url_for("flood_zone_new"))
+        flash("No fue posible crear la zona", "error")
+        return redirect(url_for("flood_zone_new"))
 
 
 @login_required
-@permission_required('zona_inundable_edit')
+@permission_required('zona_inundable_update')
 def edit(id):
     return render_template("flood_zone/edit.html",
                            zone=FloodZone.with_id(id))
 
 @login_required
-@permission_required('zona_inundable_edit')
-def modify():    
-    form = NewFloodZoneForm(request.form, csrf_enabled=False)
-
-    #Verificar 
-    if (FloodZone.n_with_code(form.data.get('code')) > 0 ):
-            flash("Código de zona ya existente", "error")
-            return redirect(url_for('flood_zone_index')) 
-    
-    if (FloodZone.n_with_name(form.data.get('name')) > 0):
-            flash("Nombre de zona ya existente", "error")
-            return redirect(url_for('flood_zone_index')) 
-    id = request.form['modify_id']
-
+def _newZone(form):
     data = request.form
     if (form.validate()):
         data.to_dict(flat=False)
-        flood_zone= FloodZone()
-        form.populate_obj(flood_zone)
-        flood_zone.color=flood_zone.color.replace("#","").upper()  #revisar, el caracter # debe ser eliminado antes
-
-        try:
-            FloodZone.destroy(FloodZone.with_id(id))
-        except ValueError as e:
-            flash(e, 'error')
-        if FloodZone.with_id(id)!= None:         
-            flash("No fue posible modificar la zona", "error")
-            return redirect(url_for("flood_zone_index"))
-        else:
-            flood_zone.save()
-            if FloodZone.with_id(flood_zone.id)!= None:
-                flash("Se modificó la zona "+flood_zone.code+" correctamente", "success")
-                return redirect(url_for("flood_zone_index"))
-            else:
-                flash("No fue posible modificar la zona", "error")
-                return redirect(url_for("flood_zone_index"))
-        
+        zone= FloodZone()
+        form.populate_obj(zone)
+        zone.color=zone.color.replace("#","").upper()  #revisar, el caracter # debe ser eliminado antes
+        return zone
     else:
         for error in form.errors:
             flash(form.errors[error], "error")
+        redirect(url_for("flood_zone_index"))
 
-    return render_template("flood_zone/show.html",
-                           zone=FloodZone.with_id(id))
+
+@login_required
+@permission_required('zona_inundable_update')
+def modify():    
+    form = NewFloodZoneForm(request.form, csrf_enabled=False)
+    oldZone=FloodZone.with_id(request.form['modify_id']);
+    
+    if FloodZone.n_with_code(form.data.get('code')):
+        if (FloodZone.with_code(form.data.get('code')).id != oldZone.id):
+            flash("Código de zona ya existente", "error")
+            return redirect(url_for('flood_zone_show', id=oldZone.id)) 
+    
+    if FloodZone.n_with_name(form.data.get('name')):
+        if (FloodZone.with_name(form.data.get('name')).id != oldZone.id):
+            flash("Nombre de zona ya existente", "error")
+            return redirect(url_for('flood_zone_show', id=oldZone.id))    
+
+    flood_zone=_newZone(form)
+    """
+    for aux in flood_zone.__dict__:
+        setattr (oldZone,aux,flood_zone.__dict__[aux])"""
+    
+
+    setattr (oldZone,"name",flood_zone.name)
+    setattr (oldZone,"code",flood_zone.code)
+    setattr (oldZone,"color",flood_zone.color)
+    setattr (oldZone,"status",flood_zone.status)
+
+    """for fpoint in oldZone.flood_points :
+        flood_zone.addPoint(fpoint)"""
+    try:
+        oldZone.modify()
+    except ValueError as e:
+        flash(e, 'error')   
+    if FloodZone.with_id(oldZone.id)!= None:
+        flash("Se modificó la zona "+oldZone.code+" correctamente", "success")
+    else:
+        flash("No fue posible modificar la zona", "error") 
+    
+
+    return redirect(url_for('flood_zone_index')) 
+
 
 @login_required
 @permission_required('zona_inundable_import')
