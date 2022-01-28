78671286bbf8c2fb2356c423e49623d317347d67
Oleastro, Lautaro <lauchaoleastro1@gmail.com>
2022-01-28 12:10:22 -0300

    Importacion y uso de CORS. Render de recorridos en mapa de la app publica

--- web/src/views/InfoMap.vue
+++ web/src/views/InfoMap.vue
@@ -1,18 +1,29 @@
 <template>
-  <l-map style="height: 400px" :zoom="zoom" :center="center">
-    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-marker :lat-lng="markerLatLng"></l-marker>
-  </l-map>
+  <div>
+    <h2>Recorridos de evacuación y Puntos de Encuentro</h2>
+    <l-map style="height: 400px" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <div v-for="route in routes" :key="route">
+        <l-polyline :lat-lngs="route.coordinates"></l-polyline>
+      </div>
+    </l-map>
+    <div>
+      <h3>Recorridos de evacuación</h3>
+      <div v-for="route in routes" :key="route">
+        <p>{{ route.name }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer, LPolyline } from "@vue-leaflet/vue-leaflet";
 
 export default {
   components: {
     LMap,
     LTileLayer,
-    LMarker,
+    LPolyline,
   },
   data() {
     return {
@@ -21,7 +32,22 @@ export default {
         '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       zoom: 13,
       center: [-34.92053918330889, -57.9541949099075],
+      routes: [],
     };
+  },
+  created() {
+    fetch("http://127.0.0.1:5000/api/recorridos-evacuacion/")
+      .then((response) => {
+        //console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        //console.log(json.recorridos);
+        this.routes = json.recorridos;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   },
 };
 </script>
